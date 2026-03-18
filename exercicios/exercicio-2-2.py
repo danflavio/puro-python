225bb7c851683e051c9447eadc4e8e4b8326ea3b
Daniel Flavio <danflavio@gmail.com>
2026-03-17 22:01:52 -0300

    Exercicios capitulo 2

---
+++ exercicios/exercicio-2-2.py
@@ -0,0 +1,6 @@
+#O volume de uma esfera com raio r é . Qual é o volume de uma esfera com raio 5?
+
+r = 5
+pi = 3.14
+volume = (4/3) * pi * r**3
+print('Volume da esfera:', round(volume, 2))
--- inicio.py
+++ inicio.py
@@ -2,3 +2,17 @@ print('ola mundo')
 print(0b1010)
 
 print(0xF0)
+
+print('2 * 1 / 6:', 2 * 1 / 6)
+print('(2 * 1) / 6:', (2 * 1) / 6)
+print('2 * (1 / 6):', 2 * (1 / 6))
+
+
+
+str = 'Dangas ' * 4
+print(str)
+n = 0
+42 == n
+
+
+print(42 == n)
